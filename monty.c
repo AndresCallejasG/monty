0ae5d2d9d3d0614d4bec93800d242dc5888c1984
andrescallejasg <andres.callejasg@gmail.com>
2020-05-14 14:31:16 +0000

    exit and frees

--- monty.c
+++ monty.c
@@ -11,7 +11,7 @@ cmd_data cmd;
  */
 int main(int argc, char *av[])
 {
-    
+
     FILE *fd;
     stack_t **stack = NULL;
     size_t len = 0;
@@ -62,12 +62,14 @@ void find_cmd(char *line, unsigned int line_cnt)
         tok = strtok(NULL, " \n\t");
         num = _atoi(tok);
         if (num == -1)
-        {            
+        {
             fprintf(stderr, "L%d: usage: push integer\n", line_cnt);
-			exit(EXIT_FAILURE);
+            free_dlistint(*stack);
+            free_struct();
+            exit(EXIT_FAILURE);
         }
         cmd.value = num;
-    }   
+    }
 }
 /**
  * main - monty interpreter main function
@@ -81,20 +83,19 @@ void exec_op(stack_t **stack, unsigned int line_cnt)
 {
     int i = 0;
     instruction_t op_func[] = {
-		{"push", _push}, {"pall", _pall}, {"pint", _pint}, {"pop", _pop},
-		{"swap", _swap}, {"add", _add}, {"nop", _nop}, {NULL, NULL}
-	};
+        {"push", _push}, {"pall", _pall}, {"pint", _pint}, {"pop", _pop}, {"swap", _swap}, {"add", _add}, {"nop", _nop}, {NULL, NULL}};
 
     while (op_func[i].f != NULL)
-	{
-        if(strcmp(op_func[i].opcode, cmd.op_code) == 0)
+    {
+        if (strcmp(op_func[i].opcode, cmd.op_code) == 0)
         {
             op_func[i].f(stack, line_cnt);
             return;
         }
-	
-	}
+    }
     fprintf(stderr, "L%u: unknown instruction %s\n", line_cnt, cmd.op_code);
+    free_dlistint(*stack);
+    free_struct();
     exit(EXIT_FAILURE);
 }
 /**
