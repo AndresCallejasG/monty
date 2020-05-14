077799fff64f770962df430736e09ee14ae3dc20
andrescallejasg <andres.callejasg@gmail.com>
2020-05-14 20:59:40 +0000

    add

--- op_codes2.c
+++ op_codes2.c
@@ -1,7 +1,17 @@
 #include "monty.h"
 void _add(stack_t **stack, unsigned int line_number){
-	(void) stack;
-	(void) line_number;
+	int n_head;
+
+	if (!(*stack) || !((*stack)->next))
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	n_head = (*stack)->n;
+	(*stack)->next->n += n_head;
+	delete_dnodeint_at_index(stack, 0);
+
 }
 void _nop(stack_t **stack, unsigned int line_number)
 {
