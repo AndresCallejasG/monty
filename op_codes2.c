e14e9dd6cf5fb25538b602c861063e95e552ff86
andrescallejasg <andres.callejasg@gmail.com>
2020-05-15 00:48:55 +0000

    sub

--- op_codes2.c
+++ op_codes2.c
@@ -13,10 +13,26 @@ void _add(stack_t **stack, unsigned int line_number)
 
 	n_head = (*stack)->n;
 	(*stack)->next->n += n_head;
-	delete_dnodeint_at_index(stack, 0);
+	_pop(stack, line_number);
 }
 void _nop(stack_t **stack, unsigned int line_number)
 {
 	(void)stack;
 	(void)line_number;
+}
+void _sub(stack_t **stack, unsigned int line_number)
+{
+	int n_head;
+
+	if (!(*stack) || !((*stack)->next))
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		free_dlistint(*stack);
+		free_struct();
+		exit(EXIT_FAILURE);
+	}
+
+	n_head = (*stack)->n;
+	(*stack)->next->n -= n_head;
+	_pop(stack, line_number);
 }
